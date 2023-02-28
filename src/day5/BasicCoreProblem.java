29f401786398a0cf1f802a92d291019c38d0f615
murtazamalik <murtazamalik1319@gmail.com>
2023-02-28 11:33:27 +0530

    Adding Leap Year

--- src/day5/BasicCoreProblem.java
+++ src/day5/BasicCoreProblem.java
@@ -2,29 +2,19 @@ package day5;
 
 import java.util.Scanner;
 
-//Flip Coin And Print Percentage Of Head And Tail
+//Leap Year
 public class    BasicCoreProblem {
     public static void main(String[] args) {
+        int year;
         Scanner scanner = new Scanner(System.in);
-        int head = 0;
-        int tail =0;
-        System.out.println("enter  no of times to flip coin");
-        double n = scanner.nextInt();
-        for (int i = 0; i < n; i++) {
-            float random = (float) Math.random();
-            if (random < 0.5) {
-                System.out.println("Head :" + n);
-                head++;
-            } else {
-                System.out.println("Tail :" + n);
-                tail++;
-            }
+        System.out.println("enter any year to check its leap year or not");
+        year = scanner.nextInt();
+
+        if (year %2 == 0 && year%4 == 0 && year%100 != 0 || year%400 == 0 ){
+            System.out.println(" Leap Year :" + year);
         }
-
-        float headPercent = (float) ( head/n * 100);
-        float tailPercent = (float) ( tail/n * 100);
-        System.out.println(" the percentage of head is :" + headPercent + "%");
-        System.out.println(" the percentage of tail is :" + tailPercent + "%");
-
+        else {
+            System.out.println("Not Leap Year :" + year );
+        }
     }
 }
